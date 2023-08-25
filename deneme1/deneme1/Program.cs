1b34e6c75cf54b27afba9d5b1792ec4bd250a7dc
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2023-08-25 21:18:35 +0300

    Add files via upload

---
+++ deneme1/deneme1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2;
+            string islem;
+            Console.WriteLine("Birinci sayıyı giriniz:");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz:");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nToplama---> t");
+            Console.WriteLine("Çıkarma--->c");
+            Console.WriteLine("Çarpma--->m");
+            Console.WriteLine("Bölme--->b");
+
+            Console.WriteLine("İşleminizi seçiniz :");
+            islem = Console.ReadLine();
+
+            if (islem == "b")
+                if (sayi2 != 0)
+                {
+                    float sonuc = (float)sayi1 / (float)sayi2;
+                    Console.WriteLine("sonuç: {0}", sonuc);
+                }
+            else 
+                {
+                    Console.WriteLine("Bölen sıfır olamaz");
+                }
+            else if (islem == "t")
+            {
+                Console.WriteLine("sonuç: {0}", sayi1 + sayi2);
+            }
+            else if (islem == "m")
+            {
+                Console.WriteLine("sonuç: {0}", sayi1 * sayi2);
+            }
+            else
+            {
+                    Console.WriteLine("sonuç: {0}", sayi1 - sayi2);
+            }
+
+            Console.ReadKey();  
+
+        }
+    }
+}
